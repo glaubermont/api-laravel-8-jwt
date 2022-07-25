3c99bef493d9f0273926ffb1efb98c64e476e840
Glauber <glaubermonteiro01@gmail.com>
2022-07-25 17:19:03 -0300

    Documentaries - API

---
+++ app/Http/Controllers/DocumentaryController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Documentary;
+
+use Illuminate\Http\Request;
+
+class DocumentaryController extends Controller
+{
+
+
+   
+   public function getById($idDocumentary)
+   {
+
+      $documentaries = Documentary::select('name','category','synopsis','link')
+         ->where('id_documentary', $idDocumentary)->first();
+      if ($documentaries) {
+         return response()->json($documentaries, 200);
+      } else {
+         return response()->json(['message' => 'Id Not Found!'], 404);
+      }
+   }
+
+    public function getAll()
+    {
+       $documentaries = Documentary::select('name','category','synopsis','link')
+       ->orderBy('name')
+       ->get();
+       if ($documentaries) {
+          return response()->json($documentaries, 200);
+       } else {
+          return response()->json(['message' => 'List Not found!'], 404);
+       }
+    }
+
+    public function delete($idDocumentary)
+    {
+ 
+       $count = Documentary::where('id_documentary', $idDocumentary)->delete();
+       // dd($data);
+       if ($count > 0) {
+ 
+          return response()->json(['message' => 'Successfully Deleted']);
+       } else {
+          return response()->json(['message' => 'Delete Failed']);
+       }
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DocumentaryController;
+use App\Http\Middleware\ProtectedRouteAuth;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+route::prefix('v1')->group(function(){
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::get('/documentary/getById/{id}','App\Http\Controllers\DocumentaryController@getById');
+    Route::get('/documentary/getAll','App\Http\Controllers\DocumentaryController@getAll');
+    Route::middleware([ProtectedRouteAuth::class])->group(function(){
+       // put documentary
+       Route::get('/documentary/delete/{id}','App\Http\Controllers\DocumentaryController@delete');
+        Route::post('/register', [AuthController::class, 'register']);
+        Route::post('/logout', [AuthController::class, 'logout']);
+        Route::post('/refresh', [AuthController::class, 'refresh']);
+        Route::get('/user-profile', [AuthController::class, 'userProfile']);       
+    });
+});
+
